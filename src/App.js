fe5b90603ea5438d13049482ebd259fd3fad9bf3
MrBownss <ahmadtaufik4599@gmail.com>
2023-03-05 22:19:18 +0700

    delet all

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 
 import './App.css';
-import CComponent from "./components/CComponent"
-import FComponent from "./components/FComponent"
 
 function App() {
   return (
-    <div className='App'>
-      <CComponent word= "2023"/>
-      <FComponent word= "ReactJS"/>
-    </div>
+    <>
+    
+    </>
   );
 }
 
